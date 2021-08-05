6fda8fa687279af6fb16029a6d3e8350bf2d4e68
Dmitry Pervoukhin <dvperv@gmail.com>
2021-08-06 01:42:23 +0300

    1

--- src/main/java/com/example/astrademoapi/FileData.java
+++ src/main/java/com/example/astrademoapi/FileData.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collection = "transients")
+@Document(collection = "defiles")
 public class FileData {
     @Id
     private String id;
--- src/main/java/com/example/astrademoapi/FileDataService.java
+++ src/main/java/com/example/astrademoapi/FileDataService.java
@@ -2,7 +2,6 @@ package com.example.astrademoapi;
 
 import org.bson.BsonBinarySubType;
 import org.bson.types.Binary;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
